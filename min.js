7923fae0b86fc28e7a35da76379d5e9e53f983a7
Khaled-mohamed-96 <k.aly96@yahoo.com>
2024-10-14 23:30:28 +0300

    Add files via upload

---
+++ min.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector("button");
+let heding = document.querySelector("h1");
+
+btn.onclick = function() {
+    heding.innerHTML = "You | Are | A | Donkey"
+    heding.style.background = "Red"
+}
